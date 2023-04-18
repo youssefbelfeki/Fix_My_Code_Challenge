03f8b01a443b751fad5a0b3e95cde2732f18ba4f
youssefbelfeki <youssef.belfeki@gmail.com>
2023-04-19 00:37:28 +0100

    go

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -10,6 +10,11 @@ class User():
         """ Documentation """
         self.__email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+
     @email.setter
     def email(self, value):
         """ Documentation """
@@ -17,14 +22,8 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
